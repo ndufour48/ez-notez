152d0e32d2768abc7145eeb72a27e5b9975431c8
Nicholas DuFour <nick.dufour48@gmail.com>
2023-04-12 22:13:53 -0400

    removed failsafe

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,8 +9,4 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../public/assets/notes.html"));
   });
 
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/assets/index.html"));
-  });
-  
 };
