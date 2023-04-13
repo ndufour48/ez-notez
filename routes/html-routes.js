4725306003b27c6d749afd7bddc87bc93c8c03b6
Nicholas DuFour <nick.dufour48@gmail.com>
2023-04-12 22:12:10 -0400

    path changes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,15 +2,15 @@ const path = require("path");
 
 module.exports = function (app) {
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/../public/assets/index.html"));
+    res.sendFile(path.join(__dirname, "../public/assets/index.html"));
   });
 
   app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/../public/assets/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/assets/notes.html"));
   });
 
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/assets/index.html"));
+    res.sendFile(path.join(__dirname, "../public/assets/index.html"));
   });
   
 };
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var PORT = process.env.PORT || 8080
 // Sets up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/public/assets", express.static("./public/assets"));
+app.use("/public/assets", express.static(__dirname + "/public/assets"));
 
 
 require("./routes/html-routes")(app);
