48c3ffe2c8c9152ada100a9b1458c70894a1840a
Nicholas DuFour <nick.dufour48@gmail.com>
2023-04-12 21:44:07 -0400

    failsafe URL directory

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,4 +8,9 @@ module.exports = function (app) {
   app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/../public/assets/notes.html"));
   });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/assets/index.html"));
+  });
+  
 };
